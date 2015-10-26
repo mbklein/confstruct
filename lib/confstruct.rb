2c213629d8f7fb3ace1f2ffcfe2ff07ac509297c
Jonathan Rochkind <jonathan@dnil.net>
2015-10-26 16:20:21 -0400

    tag version 1.0.2

--- lib/confstruct.rb
+++ lib/confstruct.rb
@@ -4,5 +4,5 @@ module Confstruct
   autoload :HashWithStructAccess, 'confstruct/hash_with_struct_access'
   autoload :Configuration, 'confstruct/configuration'
   
-  VERSION = '1.0.1'
+  VERSION = '1.0.2'
 end
